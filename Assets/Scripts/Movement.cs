a321bb8888c7eb4774a1c5ceedff33bb9e9c8b23
maxvzd <153630971+maxvzd@users.noreply.github.com>
2024-09-08 00:22:44 +0100

    Implemented scrolling to control speed

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,7 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private float turnSpeed;
+    private float _runModifier;
     
     private Animator _animator;
 
@@ -18,6 +19,12 @@ public class Movement : MonoBehaviour
     {
         float verticalInput = Input.GetAxis(Constants.VerticalKey);
         float horizontalInput = Input.GetAxis(Constants.HorizontalKey);
+        
+        _runModifier += Input.GetAxis("Mouse ScrollWheel");
+        _runModifier = Mathf.Clamp(_runModifier, 0.5f, 2f);
+
+        verticalInput *= _runModifier;
+        horizontalInput *= _runModifier;
         
         _animator.SetFloat(Constants.Vertical, verticalInput);
         _animator.SetFloat(Constants.Horizontal, horizontalInput);
