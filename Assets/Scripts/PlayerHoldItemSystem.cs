7fba2b207cdaeb5c35cf60130e724b5ee660be4f
maxvzd <153630971+maxvzd@users.noreply.github.com>
2024-09-10 15:14:17 +0100

    Refactored holding an item to it's own class

---
+++ Assets/Scripts/PlayerHoldItemSystem.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using RootMotion.FinalIK;
+using UnityEngine;
+
+public class PlayerHoldItemSystem : MonoBehaviour
+{
+    public bool IsHoldingItem => _isHoldingItem;
+    
+    [SerializeField] private Transform rightArmIkTarget;
+    
+    private Transform _currentlyHeldItem;
+    private bool _isHoldingItem;
+    
+    private OffsetPose _offsetPose;
+    private float _holdWeight;
+    private IEnumerator _holdWeightCoRoutine;
+    private IEnumerator _placeItemCoRoutine;
+
+    private Animator _animator;
+    private FullBodyBipedIK _ik;
+    private InteractionSystem _interactionSystem;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _ik = GetComponent<FullBodyBipedIK>();
+        _interactionSystem = GetComponent<InteractionSystem>();
+    }
+    
+    private void LateUpdate()
+    {
+        if (_isHoldingItem)
+        {
+            _offsetPose.Apply(_ik.solver, _holdWeight, _ik.transform.rotation);
+        }
+    }
+
+    public void SetCurrentlyHeldItem(Transform currentlyHeldItem)
+    {
+        _currentlyHeldItem = currentlyHeldItem;
+        _offsetPose = _currentlyHeldItem.GetComponent<OffsetPose>();
+        _isHoldingItem = true;
+        _animator.SetBool(Constants.IsHoldingItem, true);
+        StartHoldWeightCoRoutine(1);
+    }
+    
+    #region Start Coroutines
+
+    private void StartPlaceItemCoRoutine(Vector3 position, float timeInSeconds)
+    {
+        if (_placeItemCoRoutine is not null)
+        {
+            StopCoroutine(_placeItemCoRoutine);
+        }
+
+        _placeItemCoRoutine = PlaceItemRoutine(position, timeInSeconds);
+        StartCoroutine(_placeItemCoRoutine);
+    }
+    
+    private void StartHoldWeightCoRoutine(float weightToUpdateTo)
+    {
+        if (!ReferenceEquals(_holdWeightCoRoutine, null))
+        {
+            StopCoroutine(_holdWeightCoRoutine);
+        }
+
+        _holdWeightCoRoutine = UpdateHoldWeight(weightToUpdateTo, 0.5f);
+        StartCoroutine(_holdWeightCoRoutine);
+    }
+    
+    #endregion
+    
+    #region Coroutines
+    
+    private IEnumerator UpdateHoldWeight(float weightToUpdateTo, float timeInSeconds)
+    {
+        if (_holdWeight < weightToUpdateTo)
+        {
+            while (_holdWeight < weightToUpdateTo)
+            {
+                _holdWeight += Time.deltaTime * 1 / timeInSeconds;
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        else
+        {
+            while (_holdWeight > weightToUpdateTo)
+            {
+                _holdWeight -= Time.deltaTime * 1 / timeInSeconds;
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+
+    private IEnumerator PlaceItemRoutine(Vector3 position, float timeInSeconds)
+    {
+        while (_interactionSystem.IsInInteraction(FullBodyBipedEffector.RightHand))
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        rightArmIkTarget.transform.position = position + Vector3.up * 0.1f;
+        _ik.solver.rightHandEffector.target = rightArmIkTarget;
+
+        while (_ik.solver.rightHandEffector.positionWeight < 1)
+        {
+            float deltaTime = Time.deltaTime * 1 / timeInSeconds;
+
+            _ik.solver.rightHandEffector.positionWeight += deltaTime;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        _ik.solver.rightHandEffector.positionWeight = 1;
+
+        DropItem();
+
+        while (_ik.solver.rightHandEffector.positionWeight > 0)
+        {
+            float deltaTime = Time.deltaTime * 1 / timeInSeconds;
+
+            _ik.solver.rightHandEffector.positionWeight -= deltaTime;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        _ik.solver.rightHandEffector.positionWeight = 0;
+        _ik.solver.rightHandEffector.target = null;
+    }
+    
+    #endregion
+
+    public void DropItem()
+    {
+        _currentlyHeldItem.transform.SetParent(null);
+        _currentlyHeldItem.GetComponent<Rigidbody>().isKinematic = false;
+        _animator.SetBool(Constants.IsHoldingItem, false);
+        
+        _isHoldingItem = false;
+        StartHoldWeightCoRoutine(0);
+    }
+
+    public void PlaceItem(Vector3 position, float placeTime)
+    {
+        StartPlaceItemCoRoutine(position, placeTime);
+    }
+}
--- Assets/Scripts/PlayerInteractionSystem.cs
+++ Assets/Scripts/PlayerInteractionSystem.cs
@@ -1,28 +1,15 @@
 using System;
-using System.Collections;
 using RootMotion.FinalIK;
 using UnityEngine;
 
 public class PlayerInteractionSystem : MonoBehaviour
 {
     [SerializeField] private Transform lookAtBase;
-    [SerializeField] private Transform rightArmIkTarget;
 
-    private OffsetPose _offsetPose;
-    private InteractionSystem _interactionSystem;
-    private FullBodyBipedIK _ik;
     private Transform _lookAtTarget;
-    private Transform _currentlyHeldItem;
-    private bool _isHoldingItem;
-    private float _holdWeight;
-    private Animator _animator;
-
-
-    private IEnumerator _holdWeightEnumerator;
-    private IEnumerator _placeItemCoRoutine;
-
-
-    // Start is called before the first frame update
+    private InteractionSystem _interactionSystem;
+    private PlayerHoldItemSystem _playerHoldItemSystem;
+    
     private void Start()
     {
         int childCount = lookAtBase.childCount;
@@ -38,9 +25,8 @@ public class PlayerInteractionSystem : MonoBehaviour
 
         if (ReferenceEquals(_lookAtTarget, null)) throw new Exception("Couldn't find lookAtTarget");
 
+        _playerHoldItemSystem = GetComponent<PlayerHoldItemSystem>();
         _interactionSystem = GetComponent<InteractionSystem>();
-        _ik = GetComponent<FullBodyBipedIK>();
-        _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -54,7 +40,7 @@ public class PlayerInteractionSystem : MonoBehaviour
             float distance = Vector3.Distance(targetPosition, basePosition);
             Ray sphereRay = new Ray(basePosition, direction);
 
-            if (!_isHoldingItem)
+            if (!_playerHoldItemSystem.IsHoldingItem)
             {
                 if (Physics.SphereCast(sphereRay, 0.1f, out RaycastHit hit, distance, LayerMask.GetMask("Item")))
                 {
@@ -64,12 +50,7 @@ public class PlayerInteractionSystem : MonoBehaviour
                     if (ReferenceEquals(interactionObject, null)) return;
 
                     _interactionSystem.StartInteraction(FullBodyBipedEffector.RightHand, interactionObject, true);
-
-                    _currentlyHeldItem = pickedUpItem;
-                    _offsetPose = pickedUpItem.GetComponent<OffsetPose>();
-                    _isHoldingItem = true;
-                    _animator.SetBool(Constants.IsHoldingItem, true);
-                    StartHoldWeightCoRoutine(1);
+                    _playerHoldItemSystem.SetCurrentlyHeldItem(pickedUpItem);
                 }
             }
             else
@@ -79,108 +60,14 @@ public class PlayerInteractionSystem : MonoBehaviour
                 {
                     if (hit.normal == Vector3.up)
                     {
-                        StartPlaceItemCoRoutine(hit.point, 0.5f);
+                        //StartPlaceItemCoRoutine(hit.point, 0.5f);
+                        _playerHoldItemSystem.PlaceItem(hit.point, 0.5f);
                         return;
                     }
                 }
-                DropItem();
+
+                _playerHoldItemSystem.DropItem();
             }
         }
-    }
-
-    private void StartPlaceItemCoRoutine(Vector3 position, float timeInSeconds)
-    {
-        if (_placeItemCoRoutine is not null)
-        {
-            StopCoroutine(_placeItemCoRoutine);
-        }
-
-        _placeItemCoRoutine = PlaceItem(position, timeInSeconds);
-        StartCoroutine(_placeItemCoRoutine);
-    }
-
-    private IEnumerator PlaceItem(Vector3 position, float timeInSeconds)
-    {
-        while (_interactionSystem.IsInInteraction(FullBodyBipedEffector.RightHand))
-        {
-            yield return new WaitForEndOfFrame();
-        }
-
-        rightArmIkTarget.transform.position = position + Vector3.up * 0.1f;
-        _ik.solver.rightHandEffector.target = rightArmIkTarget;
-
-        while (_ik.solver.rightHandEffector.positionWeight < 1)
-        {
-            float deltaTime = Time.deltaTime * 1 / timeInSeconds;
-
-            _ik.solver.rightHandEffector.positionWeight += deltaTime;
-
-            yield return new WaitForEndOfFrame();
-        }
-
-        _ik.solver.rightHandEffector.positionWeight = 1;
-
-        DropItem();
-
-        while (_ik.solver.rightHandEffector.positionWeight > 0)
-        {
-            float deltaTime = Time.deltaTime * 1 / timeInSeconds;
-
-            _ik.solver.rightHandEffector.positionWeight -= deltaTime;
-
-            yield return new WaitForEndOfFrame();
-        }
-
-        _ik.solver.rightHandEffector.positionWeight = 0;
-        _ik.solver.rightHandEffector.target = null;
-    }
-
-    private void DropItem()
-    {
-        _currentlyHeldItem.transform.SetParent(null);
-        _currentlyHeldItem.GetComponent<Rigidbody>().isKinematic = false;
-        _isHoldingItem = false;
-        _animator.SetBool(Constants.IsHoldingItem, false);
-        StartHoldWeightCoRoutine(0);
-    }
-
-    private void StartHoldWeightCoRoutine(float weightToUpdateTo)
-    {
-        if (!ReferenceEquals(_holdWeightEnumerator, null))
-        {
-            StopCoroutine(_holdWeightEnumerator);
-        }
-
-        _holdWeightEnumerator = UpdateHoldWeight(weightToUpdateTo, 0.5f);
-        StartCoroutine(_holdWeightEnumerator);
-    }
-
-    private IEnumerator UpdateHoldWeight(float weightToUpdateTo, float timeInSeconds)
-    {
-        if (_holdWeight < weightToUpdateTo)
-        {
-            while (_holdWeight < weightToUpdateTo)
-            {
-                _holdWeight += Time.deltaTime * 1 / timeInSeconds;
-                yield return new WaitForEndOfFrame();
-            }
-        }
-        else
-        {
-            while (_holdWeight > weightToUpdateTo)
-            {
-                _holdWeight -= Time.deltaTime * 1 / timeInSeconds;
-                yield return new WaitForEndOfFrame();
-            }
-        }
-    }
-
-    private void LateUpdate()
-    {
-        if (_isHoldingItem)
-        {
-            _offsetPose.Apply(_ik.solver, _holdWeight, _ik.transform.rotation);
-        }
-        //_ik.solver.rightHandEffector.positionOffset += cameraTransform.rotation * holdOffset * _holdWeight;
     }
 }
